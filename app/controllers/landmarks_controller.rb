c694316858b59dc88bf0c87f53a41fa2ec96ce28
ioreka <lucymhodges@gmail.com>
2018-06-28 09:30:54 +0100

    created join table and landmark and title models, plus started landmarks controller

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,37 @@
 class LandmarksController < ApplicationController
 
+  get "/landmarks" do
+    @landmarks = Landmark.all
+    erb :"landmark/index"
+  end
+
+  get "/landmarks/new" do
+    @figures = Figure.all
+    erb :"landmark/new"
+  end
+
+  get "/landmarks/:id" do
+    erb :"landmark/show"
+  end
+
+  post "/landmarks" do
+
+  end
+
+  get "/landmarks/:id/edit" do
+    erb :"landmark/edit"
+  end
+
+  patch "/landmarks/:id" do
+
+  end
+
+  delete "/landmarks/:id" do
+    landmark = Landmark.find(params[:id])
+    landmark.destroy
+    redirect "/landmarks"
+  end
+
+
+
 end
