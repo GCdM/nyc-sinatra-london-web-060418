dc4ca26bfa39d1cd5d560b6c0a97964d2257ed1a
ioreka <lucymhodges@gmail.com>
2018-06-28 10:32:35 +0100

    added landmark controller and landmark views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,35 +3,4 @@ class ApplicationController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
 
-  get '/' do
-
-  end
-
-  get '/figures' do
-
-  end
-
-  get '/figures/new' do
-
-  end
-
-  get '/figures/:id' do
-
-  end
-
-  get '/figures/:id/edit' do
-
-  end
-
-  post '/figures' do
-
-  end
-
-  patch '/figures/:id' do
-
-  end
-
-  delete '/figures' do
-
-  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -2,28 +2,34 @@ class LandmarksController < ApplicationController
 
   get "/landmarks" do
     @landmarks = Landmark.all
-    erb :"landmark/index"
+    erb :"/landmark/index"
   end
 
   get "/landmarks/new" do
     @figures = Figure.all
-    erb :"landmark/new"
+    erb :"/landmark/new"
   end
 
   get "/landmarks/:id" do
-    erb :"landmark/show"
+    @landmark = Landmark.find(params[:id])
+    erb :"/landmark/show"
   end
 
   post "/landmarks" do
-
+    landmark = Landmark.create(params[:landmark])
+    redirect "/landmarks/"
   end
 
   get "/landmarks/:id/edit" do
-    erb :"landmark/edit"
+    @landmark = Landmark.find(params[:id])
+    @figures = Figure.all
+    erb :"/landmarks/edit"
   end
 
   patch "/landmarks/:id" do
-
+    landmark = Landmark.find(params[:id])
+    landmark.update(params[:landmark])
+    redirect "/landmarks/#{landmark.id}"
   end
 
   delete "/landmarks/:id" do
